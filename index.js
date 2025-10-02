9795ee13f1cd736766677949f6b0c606ebf7baf0
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:35:15 +0300

    Add data type examples

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-var foo;
-let bar;
-console.log(foo);
-console.log(bar);
-
-foo = 'Hello'; // old way don't use! hoisting
-bar = 'World';
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+let text = 'Hello world';
+console.log(text);
+console.log(typeof text);
+text = 'H'; // char
+console.log(text);
+console.log(typeof text);
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+let array = [1, 2, 'hello', 'world', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+let obj = {
+    name: 'Kaspar',
+    age: 32,
+    hairColor: 'Blonde',
+    isMale: true,
+};
+console.log(obj);
+console.log(typeof obj);
