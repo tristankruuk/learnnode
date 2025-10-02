e2e31b7088dbc10c7e513bc2b7e4d07754d57dfe
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:38:48 +0300

    Add function examples

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-let obj = {
-    name: 'Kaspar',
-    age: 32,
-    sexuality: 'bi',
-    fruits: ['mango', 'apple', 'cherry'],
-    cat: {
-        age: 7,
-        name: 'Nuustik',
-        gender: 'Female'
+function hello() {
+    console.log('Hello world!');
+}
+
+hello();
+hello();
+
+function helloName(name) {
+    console.log(`Hello ${name}!`);
+}
+
+helloName('Kaspar');
+helloName('Martin');
+helloName('Manivald');
+
+function hi(name='Nameless', age='Infinite') {
+    console.log(`Hi ${name} you are ${age} years old`);
+}
+hi('Kaspar', 32);
+hi('Martin', 69);
+hi('Manivald', 1000);
+hi();
+
+let sum = function (a, b) {
+    return a+b;
+}
+
+let answer = sum(4, 5);
+console.log(answer);
+console.log(sum(6, 10));
+sum(5, 6);
+
+let sub = (a, b) => {
+    return a-b;
+}
+
+console.log(sub(6, 10));
+
+let mult = (a, b) => a*b;
+
+console.log(mult(6, 10));
+
+let root = a => a*a;
+
+console.log(root(6));
+
+let cool = () => console.log('cool');
+
+cool();
+
+let awesome = {
+    party: function () {
+        console.log('PARTY!');
     },
-    parentName: 'Urmas',
-    a2sd: 'asd',
-    // parent-name: 'asd', Not allowed bcs -
-    //2asd: 'asd', Not allowed to start with number
-    'parent-name': 'Name',
-    '2asd': 'asd',
-    'some thing': 'asd2',
+    cool: () => console.log('cool'),
+    nice() {
+        console.log('nice');
+    }
 };
 
-console.log(obj);
-console.log(obj.name);
-console.log(obj.fruits[1]);
-console.log(obj.cat.name);
-console.log(obj['parent-name']);
-console.log(Object.keys(obj));
+awesome.party();
+awesome.cool();
+awesome.nice();
