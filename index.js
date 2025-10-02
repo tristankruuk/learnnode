bbfe1a5f2993e68b27b9328295f4da5d4c68c0c9
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:39:48 +0300

    Add if and switch examples

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-let bool = true;
-console.log(bool);
-bool = false;
-console.log(bool);
-bool = true && false; // AND
-bool = false && true;
-bool = false && false;
-bool = true && true;
-console.log(bool);
-bool = true || false; // OR
-bool = false || true; // OR
-bool = false || false; // OR
-bool = true || true; // OR
-console.log(bool);
-bool = !true; //NOT
-console.log(bool);
-bool = !false; //NOT
-console.log(bool);
+let num = 1;
+if (num > 10) {
+  console.log("Bigger than 10");
+} else if (num === 10) {
+  console.log("Equal to 10");
+} else {
+  console.log("Smaller than 10");
+}
 
-bool = true;
-bool = (true && false || true) && !(false || true && false);
-console.log(bool);
+let day = new Date("2025-09-25").getDay();
+console.log(day);
 
-bool = 10 > 5;
-bool = 10 < 5;
-bool = 10 > 10;
-bool = 10 < 10;
-bool = 10 == 10;
-bool = '10' == 10; // non strict will convert and check 
-bool = '10' === 10;// strict will check if types match before values
-bool = 10 >= 10;
-bool = 10 <= 10;
-bool = '10' != 10;
-bool = '10' !== 10;
-bool = [] == '';
-bool = [1,2,3] == 1;
-console.log(bool);
+if (day === 1) {
+  console.log("Esmaspäev");
+} else if (day === 2) {
+  console.log("Teisipäev");
+} else if (day === 3) {
+  console.log("Kolmapäev");
+} else if (day === 4) {
+  console.log("Neljapäev");
+} else if (day === 5 || day === 6) {
+  console.log("Pidupäev");
+} else if (day === 0) {
+  console.log("Pühapäev");
+} else {
+  console.log("Imelikpäev");
+}
+
+switch (day) {
+  case 1:
+    console.log("Esmaspäev");
+    break;
+  case 2:
+    console.log("Teisipäev");
+    break;
+  case 3:
+    console.log("Kolmapäev");
+    break;
+  case 4:
+    console.log("Neljapäev");
+    break;
+  case 5:
+  case 6:
+    console.log("Pidupäev");
+    break;
+  case 0:
+    console.log("Pühapäev");
+    break;
+  default: 
+    console.log("Imelikpäev");
+}
