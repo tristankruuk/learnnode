cb4b6f67eb10995f85a895596bd8da2a088e7096
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:39:15 +0300

    Add boolean examples

--- index.js
+++ index.js
@@ -1,63 +1,37 @@
-function hello() {
-    console.log('Hello world!');
-}
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool);
+bool = true && false; // AND
+bool = false && true;
+bool = false && false;
+bool = true && true;
+console.log(bool);
+bool = true || false; // OR
+bool = false || true; // OR
+bool = false || false; // OR
+bool = true || true; // OR
+console.log(bool);
+bool = !true; //NOT
+console.log(bool);
+bool = !false; //NOT
+console.log(bool);
 
-hello();
-hello();
+bool = true;
+bool = (true && false || true) && !(false || true && false);
+console.log(bool);
 
-function helloName(name) {
-    console.log(`Hello ${name}!`);
-}
-
-helloName('Kaspar');
-helloName('Martin');
-helloName('Manivald');
-
-function hi(name='Nameless', age='Infinite') {
-    console.log(`Hi ${name} you are ${age} years old`);
-}
-hi('Kaspar', 32);
-hi('Martin', 69);
-hi('Manivald', 1000);
-hi();
-
-let sum = function (a, b) {
-    return a+b;
-}
-
-let answer = sum(4, 5);
-console.log(answer);
-console.log(sum(6, 10));
-sum(5, 6);
-
-let sub = (a, b) => {
-    return a-b;
-}
-
-console.log(sub(6, 10));
-
-let mult = (a, b) => a*b;
-
-console.log(mult(6, 10));
-
-let root = a => a*a;
-
-console.log(root(6));
-
-let cool = () => console.log('cool');
-
-cool();
-
-let awesome = {
-    party: function () {
-        console.log('PARTY!');
-    },
-    cool: () => console.log('cool'),
-    nice() {
-        console.log('nice');
-    }
-};
-
-awesome.party();
-awesome.cool();
-awesome.nice();
+bool = 10 > 5;
+bool = 10 < 5;
+bool = 10 > 10;
+bool = 10 < 10;
+bool = 10 == 10;
+bool = '10' == 10; // non strict will convert and check 
+bool = '10' === 10;// strict will check if types match before values
+bool = 10 >= 10;
+bool = 10 <= 10;
+bool = '10' != 10;
+bool = '10' !== 10;
+bool = [] == '';
+bool = [1,2,3] == 1;
+console.log(bool);
