5b5ba391082b2f25d5af7a8bb9743c97657e4fa9
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:38:10 +0300

    Add object examples

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-let array = [1, 2, 3, 'asdas', true, [1, 2, 3]];
-console.log(array);
-console.log(array[2]);
-console.log(array[5][1]);
-array[3] = 'Hello';
-console.log(array);
-console.log(array.length);
-array.push('new value');
-console.log(array);
-array.push(1, 3, 4, 'asdasd', 'asdasd');
-console.log(array);
-let text = array.join('☺');
-console.log(text);
+let obj = {
+    name: 'Kaspar',
+    age: 32,
+    sexuality: 'bi',
+    fruits: ['mango', 'apple', 'cherry'],
+    cat: {
+        age: 7,
+        name: 'Nuustik',
+        gender: 'Female'
+    },
+    parentName: 'Urmas',
+    a2sd: 'asd',
+    // parent-name: 'asd', Not allowed bcs -
+    //2asd: 'asd', Not allowed to start with number
+    'parent-name': 'Name',
+    '2asd': 'asd',
+    'some thing': 'asd2',
+};
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.fruits[1]);
+console.log(obj.cat.name);
+console.log(obj['parent-name']);
+console.log(Object.keys(obj));
