1a96a0280106af60cb6698e852015d351a5f2a71
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-02 11:40:49 +0300

    Add loops examples

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
-let num = 1;
-if (num > 10) {
-  console.log("Bigger than 10");
-} else if (num === 10) {
-  console.log("Equal to 10");
-} else {
-  console.log("Smaller than 10");
+let sum = 0;
+for(let i=0; i<=1_000_000 ;i++) {
+    sum += i;
+}
+console.log(sum);
+
+for(let i = 10; i>=0; i--) {
+    console.log(i);
 }
 
-let day = new Date("2025-09-25").getDay();
-console.log(day);
-
-if (day === 1) {
-  console.log("Esmaspäev");
-} else if (day === 2) {
-  console.log("Teisipäev");
-} else if (day === 3) {
-  console.log("Kolmapäev");
-} else if (day === 4) {
-  console.log("Neljapäev");
-} else if (day === 5 || day === 6) {
-  console.log("Pidupäev");
-} else if (day === 0) {
-  console.log("Pühapäev");
-} else {
-  console.log("Imelikpäev");
+for(let i=1; i<1_000_000; i*=2) {
+    console.log(i);
 }
 
-switch (day) {
-  case 1:
-    console.log("Esmaspäev");
-    break;
-  case 2:
-    console.log("Teisipäev");
-    break;
-  case 3:
-    console.log("Kolmapäev");
-    break;
-  case 4:
-    console.log("Neljapäev");
-    break;
-  case 5:
-  case 6:
-    console.log("Pidupäev");
-    break;
-  case 0:
-    console.log("Pühapäev");
-    break;
-  default: 
-    console.log("Imelikpäev");
+let time = Date.now();
+let num = 0;
+while(Date.now() < time+1000) {
+    num++;
+}
+console.log(num);
+
+num = 10;
+while(num < 10) {
+    console.log('WHILE');
 }
 
-let days = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Pidupäev', 'Pidupäev'];
-console.log(days[day]);
-
-num = 11;
-answer = num > 10 ? 'Suurem' : 'Väiksem'; // shorthand if
-console.log(answer);
-
-let value = 'asdads';
-answer = value ?? 'Something else'; // null coalescing operator; do value if exists else other side
-console.log(answer);
+do {
+    console.log('DO'); 
+} while(num < 10);
