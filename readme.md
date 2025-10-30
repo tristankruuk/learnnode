51dea4825466155e00251e9ef81c268e0e8e30e3
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-10-30 19:24:44 +0200

    Add Tabs component

---
+++ readme.md
@@ -0,0 +1,34 @@
+**VUE**
+*Ülesehitus:*
+
+*<script template>JavaScript</script>*
+*<template>HTML</template>*
+
+-> Põhimõtteliselt HTML ja JavaScript samas failis, kus saab HTML'i lisada {{ muutuja }} kujul JavaScripti.
+-> Vue failide vahel saab suhelda JS'iga > import x from y
+-> Vue's saab siduda lihtsamaid JS funktsioone HTML elementidega. onClick > @click
+-> Selle abil saab luua funktsionaalseid elemente koos atribuutidega, mida saab kasutada läbi projekti.
+
+**JavaScript Node**
+
+**JavaScript Browser**
+
+**Loops (JavaScript)**
+
+**If & Switch (JavaScript)**
+
+**Boolean (JavaScript)**
+
+**Function (JavaScript)**
+
+**Object (JavaScript)**
+
+**Array (JavaScript)**
+
+**Strings (JavaScript)**
+
+**Math (JavaScript)**
+
+**Data Types (JavaScript)**
+
+**Muutujad (JavaScript)**
---
+++ src/pages/Modals.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from "vue";
+import Modal from "../components/Modal.vue";
+import ImgModal from "../components/ImgModal.vue";
+
+//ctrl+c, ctrl+v copyb terve rea
+let modalActive = ref(false);
+let modal2Active = ref(false);
+let modal3Active = ref(false);
+</script>
+
+<template>
+    <div class="container mt-3">
+        <button class="button is-primary mx-2" @click="modalActive = true">Open Modal</button>
+        <button class="button is-secondary mx-2" @click="modal2Active = true">Open Modal 2</button>
+        <button class="button is-secondary mx-2" @click="modal3Active = true">Open Modal 3</button>
+    </div>
+
+    <ImgModal :active="modalActive" @close="modalActive=false"
+    url="https://picsum.photos/seed/tristan/1280/960"></ImgModal>
+    <ImgModal :active="modal2Active" @close="modal2Active=false"
+    url="https://picsum.photos/seed/kruuk/1280/960"></ImgModal>
+    <Modal :active="modal3Active" @close="modal3Active=false">
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                    iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+                    <a href="#">#responsive</a>
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+        </div>
+    </Modal>
+
+</template>
