47fc7d5e43af6cb7803091d0b3ad33a80959de8a
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-12-01 18:14:26 +0200

    Boxen + Chalk ülesanne

--- bin/boxes.js
+++ bin/boxes.js
@@ -1,24 +1,68 @@
 import boxen from 'boxen';
 import chalk from 'chalk';
 
-console.log(boxen(chalk.red('unicorn'),
+console.log(boxen(chalk.black('Kast 1 - kollane'),
     {
-        padding: 1
+        borderStyle: 'single',
+        backgroundColor: 'yellow',
+        align: 'left',
+        width: 100, height: 5,
+        padding: 2, margin: 1,
     }
 ))
 
-console.log(boxen('unicorn',
+console.log(boxen(chalk.red.bgBlack('Kast 2 - punane'),
     {
-        padding: 1,
-        margin: 1,
-        borderStyle: 'doubleSingle'
+        borderStyle: 'double',
+        backgroundColor: 'red',
+        align: 'center',
+        width: 90, height: 4,
+        padding: 1.5, margin: 1,
     }
 ))
 
-console.log(boxen('unicrons love rainbows',
+console.log(boxen(chalk.ansi256(67)('Kast 3 - must'),
     {
-        title: 'magical',
+        borderStyle: 'round',
+        backgroundColor: 'black',
+        align: 'right',
+        width: 80, height: 3,
+        padding: 1,  margin: 1,
+    }
+))
+
+console.log(boxen(chalk.blueBright('Kast 4 - sinine'),
+    {
+        borderStyle: 'bold',
+        backgroundColor: 'blue',
+        title: chalk.black.bgWhite('Pealkiri 1 | vasak, bg: white'),
+        titleAlignment: 'left',
+        align: 'right',
+        width: 70, height: 5.5,
+        padding: 0.5,  margin: 1,
+    }
+))
+
+console.log(boxen('Kast 5 - helepunane',
+    {
+        borderStyle: 'singleDouble',
+        backgroundColor: 'redBright',
+        title: 'Pealkiri 2 | center',
         titleAlignment: 'center',
-        padding: 1
+        align: 'center',
+        width: 60, height: 4.5,
+        padding: 0,  margin: 1,
+    }
+))
+
+console.log(boxen(chalk.blue('Kast 6 - helesinine'),
+    {
+        borderStyle: 'doubleSingle',
+        backgroundColor: 'blueBright',
+        title: chalk.yellow('Pealkiri 3 | right, yellow'),
+        titleAlignment: 'right',
+        align: 'left',
+        width: 50, height: 3.5,
+        margin: 1,
     }
 ))
